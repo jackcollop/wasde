a2262c364007290caaadf833e1911ed494c5f220
jackcollop <jackcollop@gmail.com>
2025-09-15 15:59:15 -0500

    Update wasde_lit.py

--- wasde_lit.py
+++ wasde_lit.py
@@ -12,14 +12,9 @@ def cy(crop_year, attribute):
     fig1 = px.bar(world[(world.MarketYear == crop_year) & (world.Attribute == attribute)].pivot(index='ReleaseDate', columns='Region', values='Value').sort_index().astype(float).diff().drop(columns=['Major Exporters','Major Importers','Total Foreign','World Less China']), barmode='group', title=f'{crop_year} {attribute}', labels={attribute})
     return st.plotly_chart(fig1)
 
-tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Beginning Stocks","Production",'Imports','Domestic Use','Exports','Ending Stocks'])
 
 def execute():
-    if cy2024: 
-        with tab1:
-            cy('2024/25','Beginning Stocks')
-        with tab2:
-            cy('2024/25','Production'), cy('2024/25','Imports'), cy('2024/25', 'Domestic Use'), cy('2024/25','Exports'), cy('2024/25', 'Ending Stocks')
+    if cy2024: cy('2024/25','Production'), cy('2024/25','Imports'), cy('2024/25', 'Domestic Use'), cy('2024/25','Exports'), cy('2024/25', 'Ending Stocks')
     if cy2025: cy('2025/26','Beginning Stocks'), cy('2025/26','Production'), cy('2025/26', 'Imports'), cy('2025/26', 'Domestic Use'), cy('2025/26', 'Exports'), cy('2025/26', 'Ending Stocks')
 
 
