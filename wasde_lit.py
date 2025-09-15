3c9b45c36720b15232fc4165bb124aa0d485a01f
jackcollop <jackcollop@gmail.com>
2025-09-15 14:23:37 -0500

    Update wasde_lit.py

--- wasde_lit.py
+++ wasde_lit.py
@@ -9,7 +9,7 @@ st.subheader('World Cotton Supply and Use Historical Revisions')
 st.caption('Choose a crop year, then click Run')
 
 def cy(crop_year, attribute):
-    fig1 = px.bar(world[(world.MarketYear == crop_year) & (world.Attribute == attribute)].pivot(index='ReleaseDate', columns='Region', values='Value').sort_index().astype(float).diff(), barmode='group', title=f'{crop_year} {attribute}', labels={attribute})
+    fig1 = px.bar(world[(world.MarketYear == crop_year) & (world.Attribute == attribute)].pivot(index='ReleaseDate', columns='Region', values='Value').sort_index().astype(float).diff().drop(columns=['Major Exporters','Major Importers','Total Foreign','World Less China']), barmode='group', title=f'{crop_year} {attribute}', labels={attribute})
     return st.plotly_chart(fig1)
 
 
