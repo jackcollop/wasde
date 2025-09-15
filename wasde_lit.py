95c729cc700aa882f813d97f31437a766162a312
jackcollop <jackcollop@gmail.com>
2025-09-15 13:51:29 -0500

    Create wasde_lit.py

---
+++ wasde_lit.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import os
+import pandas as pd
+import plotly.express as px
+
+world = pd.read_csv('world.csv')
+
+st.subheader('World Cotton Supply and Use Historical Revisions')
+st.caption('Choose a crop year, then click Run')
+
+def cy(crop_year, attribute):
+    fig1 = px.bar(world[(world.MarketYear == crop_year) & (world.Attribute == attribute)].pivot(index='ReleaseDate', columns='Region', values='Value').sort_index().astype(float).diff(), barmode='group', title=f'{crop_year} {attribute}', labels={attribute})
+    return st.plotly_chart(fig1)
+
+
+def execute():
+    if cy2024: cy('2024/25','Beginning Stocks'), cy('2024/25','Production'), cy('2024/25', 'Domestic Use'), cy('2024/25', 'Ending Stocks')
+    if cy2025: cy('2025/26','Beginning Stocks'), cy('2025/26','Production'), cy('2025/26', 'Domestic Use'), cy('2025/26', 'Ending Stocks')
+
+
+
+
+
+# Sidebar configurations
+cy2024 = st.sidebar.checkbox('2024/25', key='a')
+cy2025 = st.sidebar.checkbox('2025/26', key='b')
+
+
+
+
+run_btn = st.sidebar.button('Run', on_click=execute, key='z')
